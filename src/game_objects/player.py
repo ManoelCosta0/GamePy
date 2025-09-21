dc152666db0dc050851e8bc11cb0114556e9acc8
Manoel Costa <manoelc062@gmail.com>
2025-09-21 18:15:29 -0300

    :sparkles: feat: adicionar movimentação ao jogador

--- src/game_objects/player.py
+++ src/game_objects/player.py
@@ -1,7 +1,16 @@
 # src/game_objects/player.py
 from src.game_objects.entity import Entity
-import arcade
 
 class Player(Entity):
     def __init__(self, image_path: str, scale: float, center_x: float, center_y: float):
         super().__init__(image_path, scale, center_x, center_y, max_hp=100)
+        self.velocity_x = 0
+        self.velocity_y = 0
+        self.is_moving = False
+
+    def update(self, *args, **kwargs):
+        # Lógica de atualização do jogador
+        if self.is_moving:
+            self.center_x += self.velocity_x
+            self.center_y += self.velocity_y
+        pass
--- src/views/game_view.py
+++ src/views/game_view.py
@@ -7,15 +7,43 @@ class GameView(arcade.Window):
 
     def __init__(self):
 
-        super().__init__(const.WINDOW_WIDTH, const.WINDOW_HEIGHT, const.WINDOW_TITLE)
+        super().__init__(const.WINDOW_WIDTH, const.WINDOW_HEIGHT, const.WINDOW_TITLE, resizable=True)
         self.background_color = arcade.csscolor.CORNFLOWER_BLUE
-        self.all_sprites = arcade.SpriteList()
+        self.sprite_list = arcade.SpriteList()
         self.player = None
 
     def setup(self):
-        self.player = Player("assets/sprites/player.png", scale=0.05, center_x=800, center_y=450)
-        self.all_sprites.append(self.player)
+        self.player = Player("assets/sprites/player.png", scale=0.03, center_x=800, center_y=450)
+        self.sprite_list.append(self.player)
 
     def on_draw(self):
         self.clear()
-        self.all_sprites.draw()
+        self.sprite_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        """Chamado sempre que uma tecla é pressionada."""
+        if key == arcade.key.W:
+            self.player.is_moving = True
+            self.player.velocity_y = const.MOVEMENT_SPEED
+        elif key == arcade.key.S:
+            self.player.is_moving = True
+            self.player.velocity_y = -const.MOVEMENT_SPEED
+        elif key == arcade.key.A:
+            self.player.is_moving = True
+            self.player.velocity_x = -const.MOVEMENT_SPEED
+        elif key == arcade.key.D:
+            self.player.is_moving = True
+            self.player.velocity_x = const.MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Chamado quando uma tecla é liberada."""
+        if key == arcade.key.W or key == arcade.key.S:
+            self.player.is_moving = False
+            self.player.velocity_y = 0
+        elif key == arcade.key.A or key == arcade.key.D:
+            self.player.is_moving = False
+            self.player.velocity_x = 0
+        pass
+
+    def on_update(self, delta_time):
+        self.sprite_list.update()
