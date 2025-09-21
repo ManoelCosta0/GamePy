68ad766921b41c4169c9c95149cadc01528e3d77
Manoel Costa <manoelc062@gmail.com>
2025-09-20 21:44:30 -0300

    :sparkles: feat: carregar sprite do jogador

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from src.views.game_view import GameView
 def main():
     window = GameView()
     window.setup()
+    window.maximize()
     arcade.run()
 
 if __name__ == "__main__":
--- src/views/game_view.py
+++ src/views/game_view.py
@@ -1,17 +1,21 @@
 import arcade
 
 from src import constants as const
+from src.game_objects.player import Player
 
 class GameView(arcade.Window):
 
     def __init__(self):
 
         super().__init__(const.WINDOW_WIDTH, const.WINDOW_HEIGHT, const.WINDOW_TITLE)
-
         self.background_color = arcade.csscolor.CORNFLOWER_BLUE
+        self.all_sprites = arcade.SpriteList()
+        self.player = None
 
     def setup(self):
-        pass
+        self.player = Player("assets/sprites/player.png", scale=0.05, center_x=800, center_y=450)
+        self.all_sprites.append(self.player)
 
     def on_draw(self):
         self.clear()
+        self.all_sprites.draw()
