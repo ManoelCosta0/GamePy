523db182df9a257f61c7061b31603db720cea822
Manoel Costa <manoelc062@gmail.com>
2025-10-05 00:53:38 -0300

    :recycle: refact: Classe "Inventory" refatorada

--- src/game_objects/inventory.py
+++ src/game_objects/inventory.py
@@ -4,46 +4,25 @@ from src.game_objects.item import Item
 class Inventory():
     """Classe para gerenciar o inventário do jogador."""
     def __init__(self):
-        self.slot_items = []
-        self.equipped_items = []
+        self.items = []
 
     def add_item(self, item: Item):
         """Adiciona um item ao inventário."""
-        if self.is_on_inventory(item) and item.stack_limit > 1:
-            self.slot_items[self.find_item_index(item)].stack += 1
-            print(f"Adicionou mais uma unidade de {item.name}. Total: {self.slot_items[self.find_item_index(item)].stack}")
+        if item.stack_limit > 1 and self.find_item(item.name) is not None:
+            self.items[self.find_item(item.name)].stack += 1
         else:
-            self.slot_items.append(item)
+            self.items.append(item)
 
-    def remove_item(self, index: int):
+    def remove_item(self, item: Item):
         """Remove um item do inventário."""
-        self.slot_items.pop(index)
+        self.items.remove(item)
 
-    def is_on_inventory(self, item: Item) -> bool:
+    def find_item(self, name: str) -> int:
         """Verifica se um item está no inventário."""
-        for items in self.slot_items:
-            if items.name == item.name:
-                return True 
-        return False
-
-    def find_item_index(self, item: Item) -> int:
-        for index, current_item in enumerate(self.slot_items):
-            if current_item.name == item.name:
-                return index
-        return -1
-
-    def equip_item(self, item: Item, index: int):
-        """Equipa um item do inventário."""
-        if item in self.slot_items:
-            self.equipped_items.append(item)
-            self.remove_item(index)
-        else:
-            print(f"{item.name} não está no inventário!")
-
-    def unequip_item(self, item: Item):
-        """Desequipa um item."""
-        if item in self.equipped_items:
-            self.equipped_items.remove(item)
-            self.add_item(item)
-        else:
-            print(f"{item.name} não está equipado!")
+        for item in self.items:
+            if item.name == name:
+                return item 
+        return None
+    
+    def get_items(self):
+        return self.items
